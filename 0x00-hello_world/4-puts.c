49cede608f5d6c38634f0920284c0d45e20d6f5c
Femi <femiezekiel989@gmail.com>
2023-02-16 15:53:52 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - using puts 
+ * main - using put
  * Return: 0 if successful
  */
 
 int main(void)
 {
 	printf("\"Programming is like building a multilingual puzzle");
-       	
 	return (0);
 }
