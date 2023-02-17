20e4523ce256aae504a895d29cdaf53190d2d48a
Femi <femiezekiel989@gmail.com>
2023-02-16 16:07:53 -0800

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+\**
+ * main - size o
+ * Return: 0 if successful
+ */
+
+ int main(void)
+{
+	printf("Size of a char: " sizeof(char));
+	printf("Size of a char: " sizeof(int));
+	printf("Size of a char: " sizeof(long int));
+	printf("Size of a char: " sizeof(long long int));
+	printf("Size of a char: " sizeof(float));
+}
+
