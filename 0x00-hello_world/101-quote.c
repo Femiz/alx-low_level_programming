c5de14aa6ecff74c61bad1788d72a0cde1e69057
Femi <femiezekiel989@gmail.com>
2023-02-16 17:44:32 -0800

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - project eight
  * Return: 1 if successful
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
