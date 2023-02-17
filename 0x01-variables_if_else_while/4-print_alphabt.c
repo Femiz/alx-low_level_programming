cdc6bc72c704d8d77986fed42c4e8204d2afc7e9
Femi <femiezekiel989@gmail.com>
2023-02-17 12:33:23 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print in lower case
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char n[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(n[i]);
+		putchar('\n');
+	}
+	return (0);
+}
