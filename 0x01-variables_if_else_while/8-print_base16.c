2311a4b481723aa45d8a74b353c67a5283a1f430
Femi <femiezekiel989@gmail.com>
2023-02-18 18:26:31 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print base16
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	char i = "f";
+
+	while (n < 10)
+	{	
+		putchar(n + '0');
+	}
+	while (i <= f)
+	{
+		putchar(i)
+	}
+	putchar('\n')
+	return (0);
+}
