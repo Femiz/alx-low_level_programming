143c63d4fd80b6e7b729969b7b97fa52f2233811
Femi <femiezekiel989@gmail.com>
2023-02-18 18:12:43 -0800

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	char n[24] = "zyxwvutsrqponmlkjihgfedcba";
+	char n[26] = "zyxwvutsrqponmlkjihgfedcba";
 	int i;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(n[i]);
 	}
