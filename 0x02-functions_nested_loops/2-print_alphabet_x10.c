405cc1fabcf7f43d0e8112d6210b958618719c39
Femi <femiezekiel989@gmail.com>
2023-02-22 15:55:39 -0800

    10xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void);
+int main(void)
+{
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	return (0);
+}
+
+void print_alphabet_x10(void)
+{
+	int start = 97, end = 122;
+
+	while (start <= end)
+	{
+		_putchar(start);
+		start++;
+	}
+	_putchar('\n');
+}
