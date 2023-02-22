12f7090149783843098bcbf4fe6c09278c833cfa
Femi <femiezekiel989@gmail.com>
2023-02-22 15:41:23 -0800

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,20 +6,14 @@
  * Return: 0 if successful
  */
 
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 void print_alphabet(void)
 {
-	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	int start = 97, end = 122;
 
-	for (i = 0; i <= 26; i++)
+	for (start = 97; start <= end; start++)
 	{
-		_putchar(alpha[i]);
+		_putchar(start);
 	}
+	return (0);
+}
