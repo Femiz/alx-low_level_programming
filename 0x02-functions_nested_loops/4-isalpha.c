a56e259558871732df99530ad1efaba8cf50a959
Femi <femiezekiel989@gmail.com>
2023-02-22 17:14:34 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - print check for alphabetic character
+ * @c:the number enter when calling the function
+ * Description: print the alphabet in lowercase or uppercase
+ * Return: 1  if successful
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
