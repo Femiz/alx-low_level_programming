70b513914e340cea4b8e23e2905f849063f12eb9
Femi <femiezekiel989@gmail.com>
2023-02-22 17:59:57 -0800

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _abs - print sign of a number
+ * _abs - print sign of a number
  * @a:the number enter when calling the function
- * Description: _abs resolve negative number
+ * Description: _ab
  * Return: 0  if successful
  */
 
