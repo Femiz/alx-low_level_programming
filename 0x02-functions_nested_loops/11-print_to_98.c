8da3ff34e47ffaa0f30e25d3ba0867eb2b657c2c
Femi <femiezekiel989@gmail.com>
2023-02-22 19:26:48 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print addition of number 
+ * @n: first value 
+ * Description: print sign of number
+ * Return: sum of @a and @b
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");;
+			n--;
+		}
+	}
+	else 
+	{
+		while (n <= 98)
+		{
+			printf("%d", n)
+				if (n != 98)
+					printf(", ")
+						n++
+		}
+
+	}
+	putchar ('\n');
+}
+}
+
