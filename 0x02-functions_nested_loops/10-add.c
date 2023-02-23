06ecd5d6d7f598020a737150aab2eb35319cf570
Femi <femiezekiel989@gmail.com>
2023-02-22 20:08:53 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,17 +2,15 @@
 #include <stdio.h>
 
 /**
- * add - print addition of number 
+ * add - print addition of number
  * @a: first value
- * @b: second value 
+ * @b: second value
  * Description: print sign of number
  * Return: sum of @a and @b
  */
 
 int add(int a, int b)
 {
-	int c;
-	c = a + b;
-	return (c);
+	return (a + b);
 }
 
