0c7210de74395e0b481fa6fa975eaf46c6dda6ec
Femi <femiezekiel989@gmail.com>
2023-02-22 17:47:07 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _abs - print sign of a number
+ * @:the number enter when calling the function
+ * Description: resolve negative number
+ *  * Return: 0  if successful
+ */
+
+int _abs(int)
+{
+	int a;
+
+	if (a < 0)
+	{
+		a *= -1
+		return (0);
+	}
+}
