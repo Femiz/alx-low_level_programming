6e8b694df6b65062a090308fa1775693d38d4b21
Femi <femiezekiel989@gmail.com>
2023-02-22 18:07:07 -0800

    last digt

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print sign of a number
+ * @r:the number enter when calling the function
+ * Description: print sign of number
+ * Return: 1  if successful
+ */
+
+int print_last_digit(int r)
+{
+	return (r % 10);
+}
