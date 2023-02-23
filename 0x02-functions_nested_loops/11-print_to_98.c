3716ba57f53103dab117faef2413051cb4191eeb
Femi <femiezekiel989@gmail.com>
2023-02-22 19:51:23 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 			if (n != 98)
-				printf(", ");;
+				printf(", ");
 			n--;
 		}
 	}
@@ -24,10 +24,10 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d", n)
+			printf("%d", n);
 				if (n != 98)
-					printf(", ")
-						n++
+					printf(", ");
+						n++;
 		}
 
 	}
