745ef28b5bd66c6a5956b099eb9283824e823484
Femi <femiezekiel989@gmail.com>
2023-02-23 18:40:51 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print number
+ * Description: print digit number
+ * Return: void
+ */
+
+void more_numbers(void)
+{	
+	int c, count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (c = '0'; c <= '14'; c++)
+		{
+			if (c >= 10)
+			{
+				_putchar('1');
+			}
+			else
+			{
+				_putchar(c % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
