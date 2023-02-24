fb386573f04d44373c03bb301be747dd4f7f1c73
Femi <femiezekiel989@gmail.com>
2023-02-23 16:09:02 -0800

    more function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper
+ * Description: print capital letter
+ * Return: 1 if successful and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
