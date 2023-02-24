cc55fdca834fbdf701c857b0922e5584d626ea6e
Femi <femiezekiel989@gmail.com>
2023-02-23 16:40:00 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multicaplication
+ * @a: value to use
+ * @b: value to use
+ * Description: print multiplication value
+ * Return: multiply @a and @b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
