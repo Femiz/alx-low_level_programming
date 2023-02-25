7beea7f43902d625b55a4dea81a3d8e87f340629
Femi <femiezekiel989@gmail.com>
2023-02-25 05:08:41 -0800

    number from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_numbers - print number from 0 up to 9
- * Return: void
- */
+* print_numbers - print number from 0 up to 9
+* Return: void
+*/
 
-void print_numbers(void)
+voidprint_numbers(void)
 {
-	int number;
+int number;
 
 	for (number = 48; number <= 58; number++)
 	{
-		_putchar(number); 
+		_putchar(number);
 	}
 	_putchar('\n');
 }
