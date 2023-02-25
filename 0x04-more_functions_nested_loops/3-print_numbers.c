5cb7468899fc0e0ed27746d826ce8eeec07c1bf7
Femi <femiezekiel989@gmail.com>
2023-02-25 04:53:02 -0800

    number from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 	int number;
 
-	for (number = 0; number <= 9; number++)
+	for (number = 48; number <= 58; number++)
 	{
-		_putchar(number + '0');
+		_putchar(number); 
 	}
 	_putchar('\n');
 }
