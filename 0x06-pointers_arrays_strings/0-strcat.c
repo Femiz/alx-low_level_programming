3e166357f3b727de1d16353fb635f27b686362e8
Femi <femiezekiel989@gmail.com>
2023-03-03 16:54:11 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two string
+ * @dest: first input
+ * @src: second input
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
