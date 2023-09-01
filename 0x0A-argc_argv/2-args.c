d9423117665a3d034f51cf88e9c30fd54b8aa011
Femiz <femiezekiel989@gmail.com>
2023-09-01 15:21:25 -0700

    argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - pprogram that rints all arguments it receives
+ * @argc: arguments numbers
+ * @argv: array in the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int w;
+
+	w = 0;
+
+	while (w < argc)
+	{
+		printf("%s\n", argv[i]);
+		w++;
+	}
+
+	return (0);
+}
